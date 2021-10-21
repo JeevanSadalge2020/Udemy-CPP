7dc5d8d353d3a8c965a37a1f736b18d53ad2a52a
JeevanSadalge <jeevanhrx@gmail.com>
2021-10-21 11:34:03 +0530

    sum

---
+++ Lectures/sum/sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Enter 2 numbers" << endl;
+	int a=0;
+	int b=0;
+	cin >> a >> b;
+	cout << "The sum of " << a << " and " << b << " is " << a+b;
+	return 0;
+}	
